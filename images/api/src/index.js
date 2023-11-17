e1bf33620fe58b3d87092fb4ecbf86f002082f3e
aliciachickenwings <91463251+AachalShrestha@users.noreply.github.com>
2023-11-17 11:30:52 +0100

    POST artist route

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -57,85 +57,32 @@ app.get('/artists', (req, res) => {
 
 
 //POST
-app.post('/artists', (req, res) => {
+app.post('/artists', async (req, res) => {
   const {
     name,
     age
   } = req.body;
-  const artistUUID = uuidv4()
-
-  db('artists')
-    .select('id')
-    .where('name', name)
-    .then((existingArtists) => {
-      if (existingArtists.length > 0) {
-        return res.status(409).json({
-          error: 'Artist with the same name already exists'
-        });
-      }
-
-      return db('artists')
-        .insert({
-          uuid: artistUUID,
-          name: name,
-          age: age
-        })
-        .returning('id');
+  const artistUUID = uuidv4();
+  const existingArtist = await db("artists").select("id").where("name", name);
+  if (existingArtist > 0) {
+    res.status(409).send({
+      message: "This artist already exists"
     })
-    .then((artistId) => {
-      if (artistId) {
-        res.status(201).json({
-          message: `${name} has been added with id ${artistId[0]}!`
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({
-        error: 'Unable to add a new artist'
+  } else {
+    const resp = await db("artists")
+      .insert({
+        name: name,
+        age: age,
+        uuid: artistUUID
       });
-    });
+
+    res.status(200).send(`Artist with name: ${name} has been added to the database`);
+  }
 });
 
-app.post('/songs', (req, res) => {
-  const {
-    name,
-    artist
-  } = req.body;
 
-  return db('artists')
-    .select('id')
-    .where('name', artist)
-    .then((existingArtists) => {
-      if (existingArtists.length > 0) {
-        return db('songs')
-          .insert({
-            name: name,
-            artist_id: existingArtists.id
-          })
-          .returning('id');
-      }
 
-      return res.status(409).json({
-        error: "Artist doesn't exist yet, add artist first"
-      });
 
-    })
-    .then((artist) => {
-      if (artist) {
-        res.status(201).json({
-          message: `${name} has been added with id ${artist[0]}!`,
-          data: artist
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({
-        error: 'Unable to add a new artist'
-      });
-    });
-})
 
 
 app.post('/songs', (req, res) => {

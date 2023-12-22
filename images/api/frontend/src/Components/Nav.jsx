59967a1acabc3733560185d0e1bfc9dbe148f8c0
aliciachickenwings <91463251+AachalShrestha@users.noreply.github.com>
2023-12-22 14:06:48 +0100

    FE: Files structure adjusted

---
+++ images/api/frontend/src/Components/Nav.jsx
@@ -0,0 +1,20 @@
+import '../Styles/Nav.css';
+
+
+const Navigation = () => {
+  return (
+    <div className="App">
+     <div className="Nav">
+        <div>
+          <p>Welcome</p>
+        </div>
+        <div>
+          <p>Home</p>
+          <p>Profile</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
